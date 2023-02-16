5d6a419e16bfea2e4b101991a7754f05f5401b8a
root <shamsasigri@gmail.com>
2023-02-16 07:02:39 -0800

    added print program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with PUT
+ *
+ * Return: Success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
